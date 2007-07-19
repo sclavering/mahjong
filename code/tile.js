5f801704eed5888d196bb6c771cc63f8a6f40340
Stephen Clavering <stephen@clavering.me.uk>
2007-07-19 23:46:51 +0000

    add code to setup tiles' lists of which others they prevent removal of, and counts of others that prevent their removal

--- code/tile.js
+++ code/tile.js
@@ -41,13 +41,44 @@ Tile.prototype = {
 
 // Template is a z-y-x indexed array of bools indicating tile positions
 function Grid(templateArray) {
-  const g = templateArray;
-  this._tiles = [];
-  function t(x, y, z) { return g[z][y][x] ? new Tile(x, y, z) : null; }
-  this._grid = [[[t(x, y, z) for(x in g[z][y])] for(y in g[z])] for(z in g)];
+  const ta = templateArray;
+  const all = []; // all tiles
+  function maketile(x, y, z) { return ta[z][y][x] ? all.push(new Tile(x, y, z)) : null; }
+  this._grid = [[[maketile(x, y, z) for(x in ta[z][y])] for(y in ta[z])] for(z in ta)];
+  // set up the tiles' .left etc. fields.  don't use for-in because it gives indices as strings
+  const g = this._grid, d = g.length, h = g[0].length, w = g[0][0].length;
+  for each(var t in all) {
+    // a full tile's width to the left, and optionally up or down half a tile's height
+    this._recordTileAsAdjacent(t, 'left', 'numnumRightBlockers', -2, -1, 0);
+    this._recordTileAsAdjacent(t, 'left', 'numnumRightBlockers', -2,  0, 0);
+    this._recordTileAsAdjacent(t, 'left', 'numnumRightBlockers', -2, +1, 0);
+    // as above, but to the right
+    this._recordTileAsAdjacent(t, 'right', 'numLeftBlockers', +2, -1, 0);
+    this._recordTileAsAdjacent(t, 'right', 'numLeftBlockers', +2,  0, 0);
+    this._recordTileAsAdjacent(t, 'right', 'numLeftBlockers', +2, +1, 0);
+    // either a quater covered, half covered, or directly underneath
+    this._recordTileAsAdjacent(t, 'below', 'numAboveBlockers', -1, -1, -1);
+    this._recordTileAsAdjacent(t, 'below', 'numAboveBlockers', -1,  0, -1);
+    this._recordTileAsAdjacent(t, 'below', 'numAboveBlockers', -1, +1, -1);
+    this._recordTileAsAdjacent(t, 'below', 'numAboveBlockers',  0, +1, -1);
+    this._recordTileAsAdjacent(t, 'below', 'numAboveBlockers', +1, +1, -1);
+    this._recordTileAsAdjacent(t, 'below', 'numAboveBlockers', +1,  0, -1);
+    this._recordTileAsAdjacent(t, 'below', 'numAboveBlockers', +1, -1, -1);
+    this._recordTileAsAdjacent(t, 'below', 'numAboveBlockers',  0, -1, -1);
+    this._recordTileAsAdjacent(t, 'below', 'numAboveBlockers',  0,  0, -1);
+  }
 }
 
 Grid.prototype = {
+  // Used during grid setup. Adds a tile (if it exists) to one of another's adjacency lists
+  _recordTileAsAdjacent: function(tile, listFieldName, countFieldName, dx, dy, dz) {
+    const g = this._grid, x = tile.x + dx, y = tile.y + dy, z = tile.z + dz;
+    const other = (g[z] && g[z][y] && g[z][y][x]) || null;
+    if(!other) return;
+    tile[listFieldName].push(other);
+    other[countFieldName] += 1;
+  },
+
   removePair: function(tileA, tileB) {
     if(tileA.value != tileB.value) return false;
     tileA.remove();
