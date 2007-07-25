7026bca2832e2f034ffd3fc968c1ad6a27dbb9ee
Stephen Clavering <stephen@clavering.me.uk>
2007-07-25 13:55:34 +0000

    simplify ui code by offseting layers' <canvas>es, rather than offseting when drawing into the canvas

--- code/ui.js
+++ code/ui.js
@@ -30,21 +30,29 @@ const ui = {
   },
 
   _resize: function(w, h, d) {
+    const stack = this._stack;
     while(this._stack.hasChildNodes()) this._stack.removeChild(this._stack.lastChild);
     this._dimensions = [w, h, d];
-    const pxwidth = (w + 2) * kTileHalfWidth;
-    const pxheight = (h + 2) * kTileHalfHeight;
+    const pxwidth = w * kTileHalfWidth;
+    const pxheight = h * kTileHalfHeight;
     // two contexts per layer (one for tiles, one for their edges/shadows)
-    for(var z = 0; z != d * 2; ++z) {
+    // Since the offsets used to mimic 3D are both negative, the first canvas
+    // (for the shadows of the bottom layer) gets the *most* offset.  We then
+    // only reduce it between a layer's shadow canvas and its faces canvas.
+    for(var z = 0, offset = d; z != d * 2; ++z, offset -= z % 2) {
+      // positioning <html:canvas>es in a <stack> doesn't work well, so..
+      var box = document.createElement("box");
+      box.top = offset * -kLayerYOffset;
+      box.left = offset * -kLayerXOffset;
+      this._stack.appendChild(box);
+      // and the <canvas> external (.style.*) dimensions are 0 in XUL unless set
       var canvas = document.createElementNS(HTMLns, "canvas");
-      this._stack.appendChild(canvas);
-      canvas.width = pxwidth;
-      canvas.height = pxheight;
+      box.appendChild(canvas);
+      canvas.style.width = (canvas.width = pxwidth) +"px";
+      canvas.style.height = (canvas.height = pxheight) + "px";
       this._contexts[z] = canvas.getContext("2d");
     }
-    this._stack.width = pxwidth;
-    this._stack.height = pxheight;
-    window.resizeTo(pxwidth, pxheight);
+    window.sizeToContent();
   },
 
   _draw: function(w, h, d, grid) {
@@ -61,7 +69,7 @@ const ui = {
     // draw a shadow, badly
     const ctx1 = this._contexts[tile.z * 2];
     ctx1.fillStyle = "rgba(128, 128, 128, 0.3)";
-    ctx1.fillRect(x - kLayerXOffset, y - kLayerYOffset, kTileWidth, kTileHeight);
+    ctx1.fillRect(x, y, kTileWidth, kTileHeight);
   },
 
   _drawTileFace: function(tile) {
@@ -88,10 +96,7 @@ const ui = {
   _selected: null, // a Tile, or null
 
   _getTileVisualCoords: function(tile) {
-    // +1 to x/y gives a visual border and space for the layer offsets to use up
-    const x = (tile.x + 1) * kTileHalfWidth + (tile.z + 1) * kLayerXOffset;
-    const y = (tile.y + 1) * kTileHalfHeight + (tile.z + 1) * kLayerYOffset;
-    return [x, y];
+    return [tile.x * kTileHalfWidth, tile.y * kTileHalfHeight];
   },
 
   onPairRemoved: function(tileA, tileB) {
@@ -105,7 +110,7 @@ const ui = {
     // clear tile face then clear shadow
     const ix = tile.z * 2, facectx = this._contexts[ix + 1], ectx = this._contexts[ix];
     facectx.clearRect(x, y, kTileWidth, kTileHeight);
-    ectx.clearRect(x - kLayerXOffset, y - kLayerYOffset, kTileWidth, kTileHeight);
+    ectx.clearRect(x, y, kTileWidth, kTileHeight);
   },
 
   onPairUnremoved: function(tileA, tileB) {
@@ -115,18 +120,12 @@ const ui = {
   },
 
   onclick: function(event) {
-    const pixelX = event.clientX - this._stack.boxObject.x;
-    const pixelY = event.clientY - this._stack.boxObject.y;
-    const g = game.grid, depth = g.length;
-    for(var z = depth - 1; z >= 0; --z) {
-      // reverse the layer tile-shadow offsets
-      var layerPixelX = pixelX - z * kLayerXOffset;
-      var layerPixelY = pixelY - z * kLayerYOffset;
-      // conver to logical-tile coordinates.  -1 is to match the +1 in _drawTile()
-      var x = Math.floor(layerPixelX / kTileHalfWidth) - 1;
-      var y = Math.floor(layerPixelY / kTileHalfHeight) - 1;
-      dump("click: ("+pixelX+","+pixelY+") == ("+layerPixelX+","+layerPixelY+") in layer "+z);
-      dump(" -- logical tile ("+x+","+y+","+z+")\n");
+    var pixelX = event.clientX - this._stack.boxObject.x;
+    var pixelY = event.clientY - this._stack.boxObject.y;
+    const depth = game.grid.length;
+    for(var z = depth - 1; z >= 0; --z, pixelX += kLayerXOffset, pixelY += kLayerYOffset) {
+      var x = Math.floor(pixelX / kTileHalfWidth);
+      var y = Math.floor(pixelY / kTileHalfHeight);
       var tile = game.getTileAt(x, y, z);
       if(!tile) continue;
       if(tile.isFree) {
