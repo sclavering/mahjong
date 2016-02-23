00bbcea45bc82c2c34b9d9aa0d2263262681d075
Stephen Clavering <stephen@clavering.me.uk>
2016-02-23 18:25:01 +0000

    rewrite layouts.js to be simpler and to tolerate the indentaion layout_data.js now has

--- code/layouts.js
+++ code/layouts.js
@@ -1,19 +1,4 @@
 const layouts = {
-  // values of FOO for which chrome://mozjong/content/layouts/FOO is a layout file
-  _layouts: [
-    'test-simple',
-    'test-towers',
-    'test-brokentowers',
-    'test-minipyramid',
-    // real layouts
-    'easy',
-    'castle',
-    'cloud',
-    'confoundingcross',
-    'glyph',
-    'pyramidswalls',
-  ],
-
   // layout-id -> template map.  templates are z-y-x-indexed arrays of booleans
   _gridTemplates: {},
 
@@ -22,35 +7,22 @@ const layouts = {
   },
 
   init: function() {
-    const req = new XMLHttpRequest();
-    const texts = {};
-    for each(var id in this._layouts) {
-      req.open("GET", 'chrome://mozjong/content/layouts/' + id, false); //synchronous
-      req.send(null);
-      dump("loaded:\n==========\n"+req.responseText+"\n===========\n");
-      texts[id] = req.responseText;
+    for(let id in layout_data) this._gridTemplates[id] = _parse_layout(layout_data[id]);
+
+    function _parse_layout(txt) {
+      txt = txt.trim();
+      const layers = txt.split("\n\n").map(layer => layer.split("\n").map(s => s.trim()));
+      return layers.map(l => _parse_layer(l, layers[0].length, layers[0][0].length));
     }
-    for(id in texts) this._gridTemplates[id] = this._parseLayout(texts[id]);
-  },
 
-  _parseLayout: function(txt) {
-    const linelength = txt.indexOf("\n");
-    const layerlength = txt.indexOf("\n\n") + 1;
-    const layerheight = layerlength / (linelength + 1);
-    if(layerheight != Math.floor(layerheight)) throw "layout very broken: non-integer number of rows";
-    const layerstrs = txt.split("\n\n");
-    return [this._parseLayer(i, layerstrs[i], linelength, layerheight) for(i in layerstrs)];
-  },
+    function _parse_layer(layer, expected_height, expected_width) {
+      if(layer.length !== expected_height) throw Error("bad layout");
+      return layer.map(line => _parse_line(line, expected_width));
+    }
 
-  _parseLayer: function(index, txt, layerWidth, layerHeight) {
-    const lines = [this._parseLine(line, layerWidth) for each(line in txt.split("\n"))];
-    if(lines.length != layerHeight)
-      throw "layer "+index+" in layout has height "+lines.length+" (should be "+layerHeight+")";
-    return lines;
+    function _parse_line(line, expected_width) {
+      if(line.length !== expected_width) throw Error("bad layout");
+      return [].slice.call(line).map(ch => ch !== ".");
+    }
   },
-
-  _parseLine: function(str, expectedLength) {
-    if(str.length != expectedLength) throw "bad line length in layout";
-    return [ch != "." for each(ch in str)];
-  }
-}
+};
